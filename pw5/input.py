d93f2fbbc14d5a0a21eeaf8b8985bcbef35caf41
ssjinkaido <tungxuannguyen16@gmail.com>
2021-04-24 22:15:35 +0700

    finish labwork 5

--- pw5/input.py
+++ pw5/input.py
@@ -1,51 +1,5 @@
 import math
 
-
-def showMarkInformationForSpecificCourse(courseName, students, courses, grades, stdscr):
-    courseId = ""
-    studentName = ""
-    studentGrade = ""
-    x = 0
-    y = 5
-    num = 0
-    for i in range(len(courses)):
-        if courses[i].get_course_name() == courseName:
-            courseId = courses[i].get_course_id()
-            break
-
-    for j in range(len(grades)):
-        if int(grades[j].get_course_id()) == int(courseId):
-            for k in range(len(students)):
-                if int(students[k].get_student_id()) == int(grades[j].get_student_id()):
-                    stdscr.addstr(y + num, x, "Student name: " + str(students[k].get_name()) + ' Student grade: ' + str(
-                        grades[j].get_grade()))
-                    num += 2
-                    break
-
-
-def findStudentAverageMark(studentId, marks, courses):
-    studentGrades = []
-    studentCredit = []
-    totalCredit = 0
-    totalMark = 0
-
-    for i in range(len(marks)):
-        if int(marks[i].get_student_id()) == int(studentId):
-            studentGrades.insert(len(studentGrades) - 1, marks[i].get_grade())
-            for j in range(len(courses)):
-                if (int(courses[j].get_course_id()) == int(marks[i].get_course_id())):
-                    studentCredit.insert(len(studentCredit) - 1, int(courses[j].get_course_credit()))
-                    break
-        else:
-            continue
-    for i in range(len(studentGrades)):
-        totalMark += studentGrades[i] * studentCredit[i]
-    for i in range(len(studentCredit)):
-        totalCredit += int(studentCredit[i])
-    averageGpa = totalMark / totalCredit
-    return averageGpa
-
-
 def add_students(stdscr):
     stdscr.addstr(2, 0, "id")
     studentId = int(stdscr.getstr(3, 0).decode())
--- pw5/main.py
+++ pw5/main.py
@@ -10,16 +10,16 @@ import os
 
 def main(stdscr):
     menu = ['AddStudent', 'AddCourse', 'AddMark', 'ShowStudent', 'ShowCourse', 'ShowMark', 'StudentAverageGpa',
-            'SortDescendingOrder', 'Decompress File', 'Exit']
-    students = []
-    courses = []
-    grades = []
-    averageGpa = []
+            'SortDescendingOrder','Exit']
+
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
     current_row_idx = 0
 
     print_menu(stdscr, current_row_idx)
+    if os.path.isfile('students.dat'):
+        zip_file = zipfile.ZipFile('students.dat', 'r')
+        zip_file.extractall()
 
     while 1:
         key = stdscr.getch()
@@ -34,34 +34,22 @@ def main(stdscr):
             stdscr.clear()
             curses.echo()
             stdscr.addstr(0, 0, 'Hello World')
-            m = 0
             studentId, studentName, dob = add_students(stdscr)
             student = Student(studentId, studentName, dob)
-            if (len(students) == 0):
-                students.append(student)
+            a_file = open("student.txt", "r")
 
-            for j in range(len(students)):
-                if (student == students[j]):
-                    students[j] = student
+            lines = a_file.readlines()
+            a_file.close()
+            new_file = open("student.txt", "w")
+            m = 0
+            for line in lines:
+                if (line.strip("\n").split("--")[0] != str(student.get_student_id())):
                     m = 1
-                    break
-                if (m == 0):
-                    students.append(student)
+                    new_file.write(line)
+            if (m == 1):
+                new_file.write(f"{student.get_student_id()}--{student.get_name()}--{student.get_dob()} \n")
 
-            with open("student.txt", "r+") as sf:
-                m = 0
-                for student in students:
-                    for line in sf.readlines():
-                        if str(line[0]) == str(student.get_student_id()) in line:
-                            m = 1
-                            break
-                        else:
-                            continue
-                    if m == 1:
-                        m = 0
-                        continue
-                    else:
-                        sf.write(f"{student.get_student_id()} -- {student.get_name()} -- {student.get_dob()} \n")
+            new_file.close()
 
             stdscr.getch()
         elif (key == curses.KEY_ENTER or key in [10, 13]) and current_row_idx == 1:
@@ -71,30 +59,21 @@ def main(stdscr):
             m = 0
             courseId, courseName, noOfCredit = add_courses(stdscr)
             course = Course(courseId, courseName, noOfCredit)
-            if (len(courses) == 0):
-                courses.append(course)
-            for j in range(len(courses)):
-                if (course == courses[j]):
-                    courses[j] = course
-                    m = 1
-                    break
-                if (m == 0):
-                    courses.append(course)
 
-            with open("course.txt", "r+") as sf:
-                m = 0
-                for course in courses:
-                    for line in sf.readlines():
-                        if str(line[0]) == str(course.get_course_id()) in line:
-                            m = 1
-                            break
-                        else:
-                            continue
-                    if m == 1:
-                        m = 0
-                        continue
-                    else:
-                        sf.write(f"{course.get_course_id()}--{course.get_course_name()} \n")
+            a_file = open("course.txt", "r")
+
+            lines = a_file.readlines()
+            a_file.close()
+            new_file = open("course.txt", "w")
+            m = 0
+            for line in lines:
+                if (line.strip("\n").split("--")[0] != str(course.get_course_id())):
+                    m = 1
+                    new_file.write(line)
+            if (m == 1):
+                new_file.write(f"{course.get_course_id()}--{course.get_course_name()}--{course.get_course_credit()} \n")
+
+            new_file.close()
 
             stdscr.getch()
         elif (key == curses.KEY_ENTER or key in [10, 13]) and current_row_idx == 2:
@@ -104,53 +83,36 @@ def main(stdscr):
             m = 0
             studentId, courseId, mark = add_marks(stdscr)
             grade = Mark(studentId, courseId, mark)
-            if (len(grades) == 0):
-                grades.append(grade)
-            for j in range(len(grades)):
-                if (grades[j] == grade):
-                    grades[j] = grade
-                    m = 1
-                    break
-                if (m == 0):
-                    grades.append(grade)
-            with open("mark.txt", "r+") as sf:
-                m = 0
-                for grade in grades:
-                    for line in sf.readlines():
-                        if str(line[0]) == str(grade.get_student_id()) and line.split("--")[1] == str(
-                                grade.get_course_id()):
-                            m = 1
-                            break
-                        else:
-                            continue
-                    if m == 1:
-                        m = 0
-                        continue
-                    else:
-                        sf.write(f"{course.get_course_id()}--{course.get_course_name()} \n")
+            a_file = open("course.txt", "r")
 
-                stdscr.getch()
+            lines = a_file.readlines()
+            a_file.close()
+            new_file = open("course.txt", "w")
+            m = 0
+            for line in lines:
+                if str(line.split("--")[0]) == str(grade.get_student_id()) and line.split("--")[1] == str(
+                        grade.get_course_id()):
+                    m = 1
+                    new_file.write(line)
+            if (m == 1):
+                new_file.write(f"{grade.get_student_id()}--{grade.get_course_id()}--{grade.get_grade()} \n")
+
+            new_file.close()
+            stdscr.getch()
         elif (key == curses.KEY_ENTER or key in [10, 13]) and current_row_idx == 3:
             stdscr.clear()
             curses.echo()
             stdscr.addstr(0, 0, 'Hello World')
             studentId = add_student_id(stdscr)
             with open("student.txt", "r") as sf:
-                m = 0
                 for line in sf.readlines():
-                    if str(line[0]) == str(studentId):
+                    if str(line.split("--")[0]) == str(studentId):
                         x = line.split("--")
                         stdscr.addstr(6, 0,
                                       "StudentId: " + str(x[0]) + " Student name: " + x[1] + " Dob: " + x[2])
                         break
                     else:
                         continue
-
-            # for i in range(0, len(students)):
-            #     if (students[i].get_student_id() == studentId):
-            #         stdscr.addstr(6, 0,
-            #                       "StudentId: " + str(students[i].get_student_id()) + " Student name: " + students[
-            #                           i].get_name() + " Dob: " + students[i].get_dob())
             stdscr.getch()
 
         elif (key == curses.KEY_ENTER or key in [10, 13]) and current_row_idx == 4:
@@ -160,19 +122,14 @@ def main(stdscr):
             stdscr.addstr(2, 0, "courseId")
             courseId = int(stdscr.getstr(3, 0).decode())
             with open("course.txt", "r") as sf:
-                m = 0
                 for line in sf.readlines():
-                    if str(line[0]) == str(courseId) in line:
+                    if str(line.split("--")[0]) == str(courseId) in line:
                         x = line.split("--")
                         stdscr.addstr(6, 0,
                                       "CourseId: " + str(x[0]) + " Course name: " + str(x[1]) + " Credit: " + str(x[2]))
                         break
                     else:
                         continue
-            # for i in range(0, len(courses)):
-            #     if (courses[i].get_course_id() == courseId):
-            #         stdscr.addstr(6, 0, "CourseId: " + str(courses[i].get_course_id()) + " Course name: " + courses[
-            #             i].get_course_name() + " Credit: " + str(courses[i].get_course_credit()))
             stdscr.getch()
 
         elif (key == curses.KEY_ENTER or key in [10, 13]) and current_row_idx == 5:
@@ -181,7 +138,22 @@ def main(stdscr):
             stdscr.addstr(0, 0, 'Hello World')
             stdscr.addstr(2, 0, "courseName")
             courseName = stdscr.getstr(3, 0).decode()
-            showMarkInformationForSpecificCourse(courseName, students, courses, grades, stdscr)
+            courseId = 0
+            x = 0
+            y = 5
+            num = 0
+            with open("course.txt", "r") as sf:
+                for line in sf.readlines():
+                    if line.split("--")[1] == courseName:
+                        courseId = line.split("--")[0]
+            with open("mark.txt", "r") as sf:
+                for line in sf.readlines():
+                    if line.split("--")[1] == str(courseId):
+                        stdscr.addstr(y + num, x,
+                                      "Student Id: " + str(line.split("--")[0]) + ' Student grade: ' + str(
+                                          line.split("--")[2]))
+                        num += 2
+
             stdscr.getch()
 
         elif (key == curses.KEY_ENTER or key in [10, 13]) and current_row_idx == 6:
@@ -190,8 +162,14 @@ def main(stdscr):
             stdscr.addstr(0, 0, 'Hello World')
             studentId = add_student_id(stdscr)
             stdscr.addstr(5, 0, "student average marks: ")
-            averageMark = findStudentAverageMark(studentId, grades, courses)
-            averageGpa.insert(len(averageGpa) - 1, [studentId, averageMark])
+            totalMark = 0
+            courseNum = 0
+            with open("mark.txt", "r") as sf:
+                for line in sf.readlines():
+                    if line.split("--")[0] == str(studentId):
+                        totalMark += float(line.split("--")[2])
+                        courseNum += 1
+            averageMark = totalMark / courseNum
             stdscr.addstr(7, 0, str(averageMark))
             stdscr.getch()
 
@@ -199,6 +177,26 @@ def main(stdscr):
             stdscr.clear()
             curses.echo()
             stdscr.addstr(0, 0, 'Hello World')
+            averageGpa = []
+            students = []
+            with open("student.txt", "r") as sf:
+                for line in sf.readlines():
+                    students.append(str(line.split("--")[0]))
+            with open("mark.txt", "r") as f:
+                for i in range(len(students)):
+                    print(students[i])
+                    totalMark = 0
+                    courseNum = 0
+
+                    with open("mark.txt", "r") as sf:
+                        for line in sf.readlines():
+                            if line.split("--")[0] == str(students[i]):
+                                totalMark += float(line.split("--")[2])
+                                courseNum += 1
+
+                    averageMark = totalMark / courseNum
+                    averageGpa.append([students[i], averageMark])
+
             averageGpa.sort(key=lambda x: x[1], reverse=True)
             y = 2
             x = 0
@@ -220,23 +218,6 @@ def main(stdscr):
             stdscr.getch()
 
         elif (key == curses.KEY_ENTER or key in [10, 13]) and current_row_idx == 9:
-            stdscr.clear()
-            curses.echo()
-            stdscr.addstr(0, 0, 'We will load data from compress file')
-            if os.path.isfile('students.dat'):
-                zip_file = zipfile.ZipFile('students.dat', 'r')
-                zip_file.extractall()
-                if os.path.isfile('student.txt'):
-                    f = open('student.txt', 'r').read().splitlines()
-                    m = 4
-                    for student in f:
-                        stdscr.addstr(m, 0, student)
-                        m += 1
-
-            stdscr.getch()
-
-
-        elif (key == curses.KEY_ENTER or key in [10, 13]) and current_row_idx == 10:
             break
 
         print_menu(stdscr, current_row_idx)
